30636b59ba751b9497e09920b951fd48d0734327
Michał Kuwik <Michal7074@poczta.fm>
2019-04-12 20:33:38 +0200

    add array 'words'

--- js/index.js
+++ js/index.js
@@ -11,3 +11,9 @@ const scoreDisplay = document.querySelector('h3.score'); //score in h3
 const timeDisplay = document.querySelector('h3.time'); //time in h3
 const message = document.querySelector('h5.message'); // h5 message value 'correct or bad'
 const seconds = document.querySelector('span.first-counter');// first-counter time to left
+
+const words = [
+    'czapka','mama','tata','drzewo','owoc','koc','kot','pies','praca','myszka','pluszak','łóżko','ubranie','bliźniaki','bohater','szczęście',
+    'magia','zło','dobro','palec','telewizor','dywan','komputer','programowanie','javascript','miłość','pranie','obraz','światło','prąd','plecy','dyskopatia','zegar','okulary','samochód','drzwi','gra','przegrania','wygrana','życie'
+];
+console.log(words.length);

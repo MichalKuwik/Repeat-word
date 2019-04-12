da53311f338955f76d5ee38fd63c3e4db2772f7f
Michał Kuwik <Michal7074@poczta.fm>
2019-04-12 20:54:57 +0200

    changes in index.html,changes in get DOM element timeDisplay and add function countDown

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ let isPlaying; // ,not inicjaling ,when game is runnig,when not is false
 const wordInput = document.querySelector('input'); //input type text
 const currentWord = document.querySelector('h2'); //word in h2
 const scoreDisplay = document.querySelector('h3.score'); //score in h3
-const timeDisplay = document.querySelector('h3.time'); //time in h3
+const timeDisplay = document.querySelector('span.second-counter'); //time in h3
 const message = document.querySelector('h5.message'); // h5 message value 'correct or bad'
 const seconds = document.querySelector('span.first-counter');// first-counter time to left
 
@@ -26,6 +26,8 @@ const words = [
 function init(){
     //load word from array
     showWord(words);
+    //call countdown every second
+    setInterval(countdown, 1000);
 }
 
 //function pick & show random word
@@ -34,5 +36,18 @@ function showWord(words){
     const randIndex = Math.floor(Math.random() * words.length);
     //Output random word
     currentWord.innerHTML = words[randIndex];
-    
+}
+
+//function countdown timer
+function countdown(){
+    //make sure time is not run out
+    if(time > 0){
+        //decrement
+        time--;
+    }else if(time === 0){
+        //Game is over
+        isPlaying - false;
+    }
+    //Show time in game
+    timeDisplay.innerHTML = time;
 }
